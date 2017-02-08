dc358378b510066f17dde8d5a494d29cc666962b
junwhan <junwyoo@hotmail.com>
2017-02-07 19:53:39 -0800

    hi

--- test.rb
+++ test.rb
@@ -1,5 +1,33 @@
 # Q1: Create a fizzBuzz method takes only one argument : def fizzBuzz (number)
+def fizzBuzz (num)
+  if num%15
+    puts "fizzBuzz"
+  elsif num%3
+    puts "fizz"
+  elsif num%5
+    puts "buzz"
+  else
+    puts num
+  end
+end
 
 # Q2: Create a method that prints out 'Christmas Tree' : def christmasTree (number)
+def christmasTree (n)
+  for i in 0..n-1
+    print " "*(n-i)
+    print "#"*i
+    print "#"
+    puts "#"*i
+  end
+end
 
 # Q3: Create a method that prints out total sum of numbers : def totalSum (string)
+
+def totalSum (num_string)
+  num = num_string.split(" ")
+  sum = 0
+  num.each do |x|
+    sum += x.to_i
+  end
+  sum
+end
