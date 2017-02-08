6985b54d3a0a7b09a7566e16d376e28fc6401095
Sia Lee <sialee@wifi-131-179-36-162.host.ucla.edu>
2017-02-07 20:02:56 -0800

    wk5 quiz

--- test.rb
+++ test.rb
@@ -1,5 +1,24 @@
 # Q1: Create a fizzBuzz method takes only one argument : def fizzBuzz (number)
 
+for i in 1..30
+    if i%15 == 0
+        puts "fizzbuzz"
+        elsif i%3 == 0
+        puts "fizz"
+        elsif i%5 == 0
+        puts "buzz"
+        else
+        puts i
+    end
+end
+
+
 # Q2: Create a method that prints out 'Christmas Tree' : def christmasTree (number)
 
+n = gets.chomp.to_i
+for i in 1..n
+    puts " "*(n-i)+"*"*(2*i-1)
+end
+
 # Q3: Create a method that prints out total sum of numbers : def totalSum (string)
+n = gets.chomp.to_i
