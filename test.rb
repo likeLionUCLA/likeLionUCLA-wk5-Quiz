27ca283cb977791a10063db55934952e8f84fd88
Sia Lee <sialee@Sias-MacBook-Pro.local>
2017-02-09 20:59:45 -0800

    quiz

--- test.rb
+++ test.rb
@@ -1,24 +1,48 @@
 # Q1: Create a fizzBuzz method takes only one argument : def fizzBuzz (number)
-
-for i in 1..30
-    if i%15 == 0
-        puts "fizzbuzz"
-        elsif i%3 == 0
-        puts "fizz"
-        elsif i%5 == 0
-        puts "buzz"
-        else
-        puts i
+def fizzbuzz (num)
+    for i in 1..num
+        if i%15 == 0
+            puts "fizzbuzz"
+            elsif i%3 == 0
+            puts "fizz"
+            elsif i%5 == 0
+            puts "buzz"
+            else
+            puts i
+        end
     end
 end
 
+fizzbuzz(30)
 
 # Q2: Create a method that prints out 'Christmas Tree' : def christmasTree (number)
 
+
 n = gets.chomp.to_i
-for i in 1..n
-    puts " "*(n-i)+"*"*(2*i-1)
+
+def tree (n)
+    for i in 1..n
+        puts " "*(n-i)+"*"*(2*i-1)
+    end
 end
 
+tree(n)
+
 # Q3: Create a method that prints out total sum of numbers : def totalSum (string)
-n = gets.chomp.to_i
+
+
+def output(input)
+    array = input.split (" ")
+    array.map! {
+        |x|
+        x = x.to_i
+    }
+    sum = 0
+    array.each {
+        |x|
+        sum += x
+    }
+    sum
+end
+
+output("23 142 234")
